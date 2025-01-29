7e871971d00e007d288864313f5029c5d9d7612d
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-29 17:16:39 +0000

    Update DistributorContext.js

--- src/context/DistributorContext.js
+++ src/context/DistributorContext.js
@@ -1,1 +1,15 @@
 
+import { createContext, useState } from 'react';
+import { distributors } from '../mock/distributors';
+
+export const DistributorContext = createContext();
+
+export const DistributorProvider = ({ children }) => {
+  const [distributorsData, setDistributorsData] = useState(distributors);
+
+  return (
+    <DistributorContext.Provider value={{ distributorsData, setDistributorsData }}>
+      {children}
+    </DistributorContext.Provider>
+  );
+};
