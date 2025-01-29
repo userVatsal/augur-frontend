6e1c8388d0d29379cc2fbb7681f8a88598e55574
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-29 17:21:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { DistributorProvider } from './context/DistributorContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DistributorProvider>
+      <App />
+    </DistributorProvider>
+  </React.StrictMode>
+);
 
