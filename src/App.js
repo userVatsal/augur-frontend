411233f3248d07c2e67ba7a680319f0541913bd9
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-29 17:17:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+import { DistributorProvider } from './context/DistributorContext';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  font-family: Arial, sans-serif;
+`;
+
+const App = () => {
+  return (
+    <DistributorProvider>
+      <AppContainer>
+        <Header />
+        <Dashboard />
+      </AppContainer>
+    </DistributorProvider>
+  );
+};
+
+export default App;
 
