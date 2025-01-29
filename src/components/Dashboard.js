6968754922a8129d4ad92ff10157ae8b10b3c87a
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-29 17:17:36 +0000

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,27 @@
+// src/components/Dashboard.js
+import React, { useContext } from 'react';
+import { DistributorContext } from '../context/DistributorContext';
+import DistributorCard from './DistributorCard';
+import styled from 'styled-components';
+
+const DashboardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const Dashboard = () => {
+  const { distributorsData } = useContext(DistributorContext);
+
+  return (
+    <DashboardContainer>
+      {distributorsData.map((distributor, index) => (
+        <DistributorCard key={index} distributor={distributor} />
+      ))}
+    </DashboardContainer>
+  );
+};
+
+export default Dashboard;
 
