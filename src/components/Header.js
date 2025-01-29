56dafe333c193eb2ffafbeb92d1e9b901e3edca1
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-29 17:17:11 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,21 @@
+// src/components/Header.js
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  background-color: #282c34;
+  padding: 20px;
+  color: white;
+  text-align: center;
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <h1>Augur Dashboard</h1>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
 
